5b940f8fbcd0da53618f81a76d160c74caaab0b1
Your Name <you@example.com>
2016-09-27 13:19:06 +0800

    [str]

---
+++ company/str_pad.php
@@ -0,0 +1,8 @@
+<?php
+        $code_len = strlen('888');
+        $length = 32 - $code_len;
+$new_code = str_pad('9104000100000001',$length,'0').'888';
+echo $new_code;
+$lan = strlen('91040001000000082010thumb0000000');
+echo $lan;
+?>
